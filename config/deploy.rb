3cad5520fd0e2a330edf41b872e9b6932f8da8a2
Pablo Guelfi <pguelfi@gmail.com>
2024-10-15 13:37:37 +0200

    f

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,6 @@ require 'mina/git'
 require 'mina/rvm'
 
 # Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
 set :application_name, 'app1'  # Your application name
 set :domain, '147.79.75.87'     # Your server's IP address
 set :user, fetch(:application_name)  # Username to SSH into the server
@@ -16,17 +11,11 @@ set :repository, 'git@github.com:p-guelfi/app1.git'  # GitHub repository URL
 set :branch, 'master'  # Branch to deploy
 set :rvm_use_path, '/etc/profile.d/rvm.sh'  # Path to RVM
 
-# Optional settings:
-# set :user, 'foobar'          # Username in the server to SSH to.
-# set :port, '30000'           # SSH port number.
-# set :forward_agent, true     # SSH forward_agent.
-
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Shared files and directories
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 set :shared_dirs, fetch(:shared_dirs, []).push('public/packs', 'node_modules')
 
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
+# Remote environment task
 task :remote_environment do
   ruby_version = File.read('.ruby-version').strip
   raise "Couldn't determine Ruby version: Do you have a file .ruby-version in your project root?" if ruby_version.empty?
@@ -34,7 +23,7 @@ task :remote_environment do
   invoke :'rvm:use', ruby_version
 end
 
-# Put any custom commands you need to run at setup
+# Setup task
 task :setup do
   in_path(fetch(:shared_path)) do
     command %[mkdir -p config]
@@ -48,9 +37,9 @@ task :setup do
   timeout: 5000]
     command %[test -e #{path_database_yml} || echo "#{database_yml}" > #{path_database_yml}]
 
-    # Create secrets.yml if it doesn't exist
+    # Create secrets.yml with a static secret key for testing
     path_secrets_yml = "config/secrets.yml"
-    secrets_yml = %[production:\n  secret_key_base:\n    #{`bundle exec rake secret`.strip}]
+    secrets_yml = %[production:\n  secret_key_base: 'your_static_secret_here']
     command %[test -e #{path_secrets_yml} || echo "#{secrets_yml}" > #{path_secrets_yml}]
 
     # Remove others-permission for config directory
@@ -60,11 +49,7 @@ end
 
 desc "Deploys the current version to the server."
 task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -79,5 +64,4 @@ task :deploy do
 end
 
 # For help in making your deploy script, see the Mina documentation:
-#
 #  - https://github.com/mina-deploy/mina/tree/master/docs
